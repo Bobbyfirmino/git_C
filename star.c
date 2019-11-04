49b7a6fd95dbfaf7b7aa7d4afdf0c93b3d590dde
Bobbyfirmino <maazahmedlfc@gmail.com>
2019-11-04 13:25:12 +0530

    Starry_Night

---
+++ star.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+  {
+       int n,c,r,l,p;
+
+     printf("Enter a no ");
+ 
+              scanf("%d",&n);
+   
+     for(r=n;r>=1;r--)
+  {
+    for(l=n-r;l>=1;l--)
+
+      printf(" ");
+     
+      for(c=1;c<=r;c++)
+       
+     printf(" *");
+
+   printf("\n");
+                          }
+                 }
+
